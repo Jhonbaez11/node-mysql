7d9b128e54ef1ffb4c31684c105d677a9addc8b2
jhonbaez11 <jhonbaez11@hotmail.com>
2022-08-18 19:24:20 -0500

    ndejs con mysql

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3050;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// MySql
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'node_mysql'
+});
+
+// Route
+app.get('/api/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
+// all customers
+app.get('/api/customers', (req, res) => {
+  const sql = 'SELECT * FROM customers';
+
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.get('/api/customers/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM customers WHERE id = ${id}`;
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.post('/api/add', (req, res) => {
+  const sql = 'INSERT INTO customers SET ?';
+
+  const customerObj = {
+    name: req.body.name,
+    city: req.body.city
+  };
+
+  connection.query(sql, customerObj, error => {
+    if (error) throw error;
+    res.send('Customer created!');
+  });
+});
+
+app.put('/api/update/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, city } = req.body;
+  const sql = `UPDATE customers SET name = '${name}', city='${city}' WHERE id =${id}`;
+
+  connection.query(sql, error => {
+    if (error) throw error;
+    res.send('Customer updated!');
+  });
+});
+
+app.delete('/api/delete/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `DELETE FROM customers WHERE id= ${id}`;
+
+  connection.query(sql, error => {
+    if (error) throw error;
+    res.send('Delete customer');
+  });
+});
+
+// Check connect
+connection.connect(error => {
+  if (error) throw error;
+  console.log('Database server running!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
